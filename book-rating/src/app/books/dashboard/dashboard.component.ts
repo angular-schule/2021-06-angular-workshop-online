327ebc92e98893e2c1f6c06cf63100261666f8af
Ferdinand Malcher <ferdinand@malcher.media>
2021-06-10 14:05:04 +0200

    Services und Testing

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -10,7 +11,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor() { }
+  constructor(private rs: BookRatingService) {}
 
   ngOnInit(): void {
     this.books = [
@@ -32,11 +33,18 @@ export class DashboardComponent implements OnInit {
   }
 
   onRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.rs.rateUp(book);
+    this.updateList(ratedBook);
   }
   
   onRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.rs.rateDown(book);
+    this.updateList(ratedBook);
+  }
+
+  private updateList(ratedBook: Book) {
+    this.books = this.books
+      .map(b => ratedBook.isbn === b.isbn ? ratedBook : b);
   }
 
 }
