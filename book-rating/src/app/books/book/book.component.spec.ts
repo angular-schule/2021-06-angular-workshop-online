6e0ffb0ca789c1097fd7ba6a8926b5814a953729
Ferdinand Malcher <ferdinand@malcher.media>
2021-06-11 10:06:35 +0200

    Test BookComponent

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Book } from '../shared/book';
 
 import { BookComponent } from './book.component';
 
@@ -17,6 +18,17 @@ describe('BookComponent', () => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance; // TS
     // const element = fixture.nativeElement; // DOM
+
+    // Buch übergeben
+    // Wichtig: vor detectChanges!
+    component.book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3,
+      price: 4
+    };
+
     fixture.detectChanges();
   });
 
@@ -24,7 +36,18 @@ describe('BookComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should emit event for onRateUp', () => {
-    // TODO
+  it('should emit event for onRateUp', () => { 
+    let emittedBook: Book | undefined;
+
+    // Event abonnieren
+    component.rateUp.subscribe(book => {
+      emittedBook = book;
+    });
+
+    // Methode aufrufen
+    component.onRateUp();
+
+    expect(emittedBook).toBeTruthy()
+    expect(emittedBook).toBe(component.book);
   });
 });
