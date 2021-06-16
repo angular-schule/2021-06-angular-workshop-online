509cbf86ecb8b7c253acbfe3bdbb73ee024dc6ec
Ferdinand Malcher <ferdinand@malcher.media>
2021-06-16 13:31:18 +0200

    Typeahead-Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, filter, switchMap } from 'rxjs/operators';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-search',
@@ -8,11 +12,23 @@ import { FormControl } from '@angular/forms';
 })
 export class BookSearchComponent implements OnInit {
 
+  results$: Observable<Book[]>;
+
   searchControl = new FormControl('');
 
-  constructor() {
-    this.searchControl.valueChanges
-      .subscribe(e => console.log(e));
+  constructor(private bs: BookStoreService) {
+    this.results$ = this.searchControl.valueChanges.pipe(
+      debounceTime<string>(1000),
+      filter(term => term.length >= 3),
+      switchMap((term) => this.bs.search(term))
+    );
+
+    /*
+    - Suchbegriff muss mindestens 3 Zeichen lang sein. (Dafür bitte keinen Validator verwenden, sondern RxJS.)
+    - erst suchen, wenn Nutzer für eine bestimmte Zeit "die Finger still hält"
+    - HTTP: Bücher suchen (`this.bs.search(term)`)
+    - Bücher darstellen, ganz simpel (AsyncPipe)
+    */
   }
 
   ngOnInit(): void {
